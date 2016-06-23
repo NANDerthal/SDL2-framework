9df540df99100d9dea0d00a88a0cfed4a6e96b09
Jeff Souza <jeffreymsouza@gmail.com>
2016-06-23 13:09:07 -0600

    Added init SDL_img and unimplemented functions

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,11 +1,25 @@
 #include "Game.h"
 
 Game::Game() {
-
 }
 
 Game::~Game() {
+	IMG_Quit();
 	SDL_Quit();
+}
+
+Window Game::newWindow( std::string winTitle, int height, int width ) {
+	Window win;
+	win.init( winTitle, height, width);
+
+	return win;
+}
+
+void Game::loadMenu() {
+	// TODO: IMPLEMENT 
+}
+
+void Game::loadLevel( std::string saveFile ) {
 }
 
 bool Game::init() {
@@ -14,8 +28,20 @@ bool Game::init() {
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 		return false;
 	}
+
+	// Initialize SDL_Image with PNG support
+	int imgFlags = IMG_INIT_PNG;
+	if ( !( IMG_Init( imgFlags ) & imgFlags ) ) {
+		printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+		return false;
+	}
 	
+	// Create window
+	mainWindow = newWindow();
+
+	// Load Menu level
+	loadMenu();
+
 	// Initialization was successful
 	return true;
 }
-
--- src/Game.h
+++ src/Game.h
@@ -1,14 +1,21 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <string>
+
 #include <SDL2/SDL.h>
 
+#include "SaveFile.h"
 #include "Window.h"
 
 class Game {
 
 private:
 	Window mainWindow;
+	SaveFile save;
+
+	Window newWindow( std::string winTitle, int height = 480, int width = 640 );
+	void loadLevel( std::string saveFile = "" );
 
 public:
 	Game();
