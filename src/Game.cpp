f40d73ee01e2d68b9da18d3503cab057e8ec5ae9
Sunny <quatridunull@gmail.com>
2016-06-23 10:40:47 -0600

    Created game class and added SDL initialization and closing to it

---
+++ src/Game.cpp
@@ -0,0 +1,17 @@
+#include "Game.h"
+
+Game::~Game() {
+	SDL_Quit();
+}
+
+bool Game::init() {
+	// Initialize SDL
+	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		return false;
+	}
+	
+	// Initialization was successful
+	return true;
+}
+
---
+++ src/Game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL2/SDL.h>
+
+class Game {
+
+private:
+
+public:
+	Game();
+	~Game();
+	
+	// initialize SDL
+	void init();
+
+};
+
+#endif
+
