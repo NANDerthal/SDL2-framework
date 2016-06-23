fa6f96e73584e1db504c735bc2eb93a2991ccbfe
Sunny <quatridunull@gmail.com>
2016-06-23 10:40:03 -0600

    Added copy constructor and moved SDL initialization/closing to another class

--- src/Window.cpp
+++ src/Window.cpp
@@ -7,34 +7,65 @@ Window::Window( int height, int width ) {
 	screenWidth = width;
 }
 
+void Window::copyWindow( const Window &source ) {
+	if ( source.getRenderer() == NULL ) {
+		window = NULL;
+		renderer = NULL;
+		screenHeight = source.getHeight();
+		screenWidth = source.getWidth();
+	}
+	else {
+		const char* title = source.getTitle();
+		init( title, source.getHeight(), source.getWidth() );
+	}
+}
+
+Window::Window( const Window &source ) {
+	copyWindow( source );
+}
+
+Window& Window::operator= ( const Window &source ) {
+	copyWindow( source );
+	return *this;
+}
+
 Window::~Window() {
 	SDL_DestroyRenderer( renderer );
 	renderer = NULL;
 	
 	SDL_DestroyWindow( window );
 	window = NULL;
-	
-	SDL_Quit();
 }
 
-int Window::getWidth() {
+int Window::getWidth() const {
 	return screenWidth;
 }
 
-int Window::getHeight() {
+int Window::getHeight() const {
 	return screenHeight;
 }
 
-SDL_Renderer* Window::getRenderer() {
-	return renderer;
+SDL_Renderer* Window::getRenderer() const {
+	if ( window == NULL ) {
+		return NULL;
+	}
+	else {
+		return renderer;
+	}
 }
 
-bool Window::init( std::string winTitle ) {
-	// Initialize SDL
-	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-		return false;
-	}	
+const char* Window::getTitle() const {
+	if ( window == NULL ) {
+		return "";
+	}
+	else {
+		return SDL_GetWindowTitle( window );
+	}
+}
+
+bool Window::init( std::string winTitle, int height, int width ) {
+	screenHeight = height;
+	screenWidth = width;	
 	
 	// Initialize window
 	window = SDL_CreateWindow( winTitle.c_str(), SDL_WINDOWPOS_UNDEFINED, 
@@ -61,7 +92,7 @@ void Window::resizeWindow( int newWidth, int newHeight ) {
 	}
 }
 
-void Window::render( ) {
+void Window::render( ) const {
 	SDL_RenderPresent( renderer );
 }
 
--- src/Window.h
+++ src/Window.h
@@ -11,25 +11,31 @@ private:
 	SDL_Renderer* renderer;
 	int screenHeight;
 	int screenWidth;
+	
+	void copyWindow( const Window &source );
 
 public:
 	Window( int height = 480, int width = 640 );
+	Window( const Window &source );
+	Window& operator= ( const Window &source );
 	~Window();
 	
 	// Read member variables
-	int getWidth();
-	int getHeight();
-	SDL_Renderer* getRenderer();
+	int getWidth() const;
+	int getHeight() const;
+	SDL_Renderer* getRenderer() const;
+	const char* getTitle() const;
 	
-	// Initialize SDL, window, and renderer
+	// Initialize window and renderer
 	// Returns true if successful, false if failed
-	bool init( std::string winTitle );
+	bool init( std::string winTitle, int height = 480, int width = 640 );
 	
 	// Set member variables and adjust window accordingly
 	void resizeWindow( int newWidth, int newHeight );
 	
 	// Draw to window whatever was in renderer
-	void render( );
+	void render( ) const;
 };
 
 #endif
+
